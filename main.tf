f12de46f69ec39993add4cfb54b7173223ab7133
Joe Hosteny <jhosteny@carnegierobotics.com>
2019-03-01 17:02:54 -0500

    again

--- main.tf
+++ main.tf
@@ -1,2 +1,10 @@
 resource "null_resource" "example" {
 }
+
+resource "aws_s3_bucket" "test" {
+  bucket_prefix = "atlantis-test"
+}
+
+resource "aws_s3_bucket" "test" {
+  bucket_prefix = "atlantis-test-again"
+}
